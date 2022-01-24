8f5083b821135f42131338f0c8897415c6dbe697
Gliese129 <3207960592@qq.com>
2022-01-24 22:15:09 +0800

    init

---
+++ src/test/kotlin/gliese/note/NoteApplicationTests.kt
@@ -0,0 +1,13 @@
+package gliese.note
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NoteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
